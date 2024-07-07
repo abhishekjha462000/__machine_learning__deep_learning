b98de0b2f421297b99f558d1a9294440004d6d27
Abhishek jha (دانيال) <46026348+sameerjha462000@users.noreply.github.com>
2024-07-07 13:52:02 +0530

    .

---
+++ logical_operators.R
@@ -0,0 +1,44 @@
+# Logical operators
+
+4 < 5
+
+4 == 5
+
+4 != 5
+
+4 <= 5
+
+4 >= 5
+
+4 > 5
+
+result <- 4 < 5
+
+result
+
+result == TRUE
+
+isTRUE(result)
+typeof(result)
+
+result2 <- !(4 < (5+2))
+
+result2
+
+# & operator
+T & T
+T & F
+F & T
+F & F
+
+# operator |
+
+T | T
+T | F
+F | T
+F | F
+
+
+
+
+
---
+++ loops.R
@@ -0,0 +1,18 @@
+# loops
+
+counter <- 1
+while(counter < 5) {
+  print(counter)
+  counter <- counter + 1
+}
+
+
+while(T){
+  print("Infinite loop")
+}
+
+5:10 # These are called vectors as in R we do not have the for loops with ;
+
+for(i in 6 : 10){
+  print("I love you ❣")
+}
