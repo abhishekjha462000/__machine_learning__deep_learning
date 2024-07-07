4b6a27103346c0bceb9289a4f8323365a3ce7f39
Abhishek jha (دانيال) <46026348+sameerjha462000@users.noreply.github.com>
2024-07-07 12:00:39 +0530

    .

---
+++ myFirstRScript.R
@@ -0,0 +1,45 @@
+# integer
+x <- 2L
+typeof(x)
+
+# double
+y <- 2.5
+typeof(y)
+
+# character
+name <- "Daniyaal Ibrahim"
+typeof('Abhishek')
+typeof(name)
+
+# logical
+typeof(T)
+typeof(F)
+typeof(FALSE)
+typeof(TRUE)
+
+# complex
+origin <- 0 + 0i
+typeof(origin)
+
+# By default the type of an integer is double
+a <- 10
+typeof(a)
+
+# using variables
+b <- 10
+c <- 23.5
+
+d <- b + c
+
+d # to print the d
+
+result <- b / c
+result
+
+answer <- sqrt(result)
+answer
+
+greeting <- "Hello my name is "
+greeting
+
+paste(greeting, name)
